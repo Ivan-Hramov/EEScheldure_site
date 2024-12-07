a6c24327cb899a8071f60e294cfdc63f90b7ebcc
IvanHramov <ivan.hramov@tptlive.ee>
2024-12-07 17:44:38 +0200

    complete contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,12 @@
+<svelte:head>
+    <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
+</svelte:head>
+
 <script>
     import { onMount } from 'svelte';
 
     let gtoken = null;
+    let wasMsgSent = false;
 
     onMount(() => {
         window.onSubmit = (token) => {
@@ -12,6 +17,10 @@
     function sendForm() {
         if (!gtoken) {
             alert('Please complete the CAPTCHA');
+            return;
+        }
+        if (wasMsgSent) {
+            alert('You have already sent a message.');
             return;
         }
         const name = document.querySelector('#name').value;
@@ -27,15 +36,16 @@
             },
             body: `name=${encodedName}&email=${encodedEmail}&message=${encodedMessage}`
         })
-        .then(res => { res.json() })
+        .then(res => res.json())
         .then(data => {
+            console.log(data);
             if (data.success) {
                 alert('Message sent successfully! Thank you <3');
                 document.querySelector('#name').value = '';
                 document.querySelector('#mail').value = '';
                 document.querySelector('#message').value = '';
                 document.querySelector('#submit-button').disabled = true;
-                gtoken = null;
+                wasMsgSent = true;
             } else {
                 alert('An error occurred while sending the message');
                 console.debug(data);
@@ -44,24 +54,19 @@
     }
 </script>
 
-<svelte:head>
-    <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
-</svelte:head>
-
-<!--TODO Адаптив -->
 <main class="clr-white tw-flex">
-    <form on:submit={() => { sendForm() }} class="clr-white tw-flex tw-flex-col">
+    <form on:submit={(event) => { event.preventDefault(); sendForm(); }} class="clr-white tw-flex tw-flex-col">
         <label for="name">Name:</label>
-        <input type="text" placeholder="Example: Ivan Hramov" id="name" required>
+        <input type="text" placeholder="Ivan Hramov" id="name" required>
 
         <label for="mail">Mail:</label>
-        <input type="email" placeholder="Example: ivan.hramov@eescheldure.ru" id="mail" required>
+        <input type="email" placeholder="ivan.hramov@eescheldure.ru" id="mail" required>
 
         <label for="message">Message:</label>
-        <textarea type="text" placeholder="Example: Hi, i wanna to work with you. Please write me." id="message" required></textarea>
+        <textarea type="text" placeholder="Hi, i wanna to work with you. Please write me." id="message" required></textarea>
 
-        <div class="submit tw-flex tw-items-center">
-            <button disabled id="submit-button" class="btn" style="width: 40%;" type="submit" value="Submit">Send</button>
+        <div class="submit tw-items-center">
+            <button disabled id="submit-button" class="btn" type="submit" value="Submit">Send</button>
             <div class="tw-flex-grow"></div>
             <div class="g-recaptcha" data-callback="onSubmit" data-theme="dark" data-action="SEND" data-sitekey="6LdTVZQqAAAAAK3laQ5e56ZEuz50CjXqfVZdGc90"></div>
         </div>
@@ -90,6 +95,12 @@
         height: 10rem;
         margin-bottom: 2rem;
     }
+    .submit {
+        display: flex;
+    }
+    #submit-button {
+        width: 40%;
+    }
 
     input:-webkit-autofill,
     input:-webkit-autofill:hover,
@@ -97,5 +108,29 @@
     input:-webkit-autofill:active{
         -webkit-box-shadow: 0 0 0 200px var(--secondary-light) inset !important;
         -webkit-text-fill-color: var(--white);
+    }
+
+    @media (max-width: 600px) {
+        #message {
+            width: 25rem;
+        }
+        .submit {
+            display: flex;
+            flex-direction: column;
+        }
+        #submit-button {
+            width: 100%;
+        }
+        .g-recaptcha {
+            margin-top: 1rem;
+        }
+    }
+    @media (max-width: 431px) {
+        #message {
+            width: 100%;
+        }
+        input {
+            width: 100%;
+        }
     }
 </style>
